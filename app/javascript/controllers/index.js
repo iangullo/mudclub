cdc827de01d4b773f7c1f2dc3dc046d8724ee2e2
iangullo <87307767+iangullo@users.noreply.github.com>
2021-09-06 12:55:47 +0200

    + Improved layout & positioning of modals. + Added responsive highlighting to modal buttons.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,19 +1,14 @@
-// Load all the controllers within this directory and all subdirectories.
-// Controller files must be named *_controller.js.
-
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
+
+// Configure Stimulus development experience
+application.warnings = true
+application.debug    = false
+window.Stimulus      = application
+
+// Import and register all your controllers within this directory and all subdirectories
+// Controller files must be named *_controller.js or *_controller.ts
+import { definitionsFromContext } from "@hotwired/stimulus"
+const context = require.context("controllers", true, /_controller\.(js|ts)$/)
 application.load(definitionsFromContext(context))
-// Import and register all TailwindCSS Components
-import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
-application.register('alert', Alert)
-application.register('autosave', Autosave)
-application.register('dropdown', Dropdown)
-application.register('modal', Modal)
-application.register('tabs', Tabs)
-application.register('popover', Popover)
-application.register('toggle', Toggle)
-application.register('slideover', Slideover)
