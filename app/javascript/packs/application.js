fac9f63ac3b946a12c0d61cca001f35b182618be
iangullo <87307767+iangullo@users.noreply.github.com>
2021-09-07 16:06:05 +0200

    + Moved from turbolinks to turbo + added actiontext

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,13 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import Turbo from "@hotwired/turbo"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "controllers"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
+
+require("trix")
+require("@rails/actiontext")
