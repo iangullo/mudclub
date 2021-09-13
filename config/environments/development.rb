6ae80185d0753883d675cd5e2d092730ea9050e2
iangullo <iangullo@gmail.com>
2021-09-13 11:56:37 +0200

    Enabled connection from local machines to development server.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Whitelist one hostname
+  config.hosts << "red"
+  # Whitelist a test domain. Rails adds \A and \z around
+  # your regular expressions.
+  config.hosts << /tsalpa\.lan/
 end
