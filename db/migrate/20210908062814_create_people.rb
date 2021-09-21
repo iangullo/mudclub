798e34039cb1719df9dfcd9128c8af445922c946
Iván Glez. Angullo <iangullo@gmail.com>
2021-09-21 10:02:29 +0200

    Improved Gemfile.lock & tried to build a better migration for create_people - not creating the empty placeholder for unknown reasons.

--- db/migrate/20210908062814_create_people.rb
+++ db/migrate/20210908062814_create_people.rb
@@ -10,6 +10,6 @@ class CreatePeople < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
-    Person.create(id: 0, dni: '0', nick: 'New', name: 'Nueva', surname: 'Persona', birthday: Date.today, female: true)
+    Person.create(id: 0, name: 'Nueva', surname: 'Persona', birthday: '2021-09-01', female: true)
   end
 end
