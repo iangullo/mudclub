c5f71e36b52f9e7db3e40269fbfea4eae28115c0
Iván Glez. Angullo <iangullo@gmail.com>
2021-09-21 16:20:38 +0200

    minor improvement in Coaches index

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -46,9 +46,9 @@ class Coach < ApplicationRecord
 	#Search field matching
 	def self.search(search)
 		if search
-			search.length>0 ? Coach.where(person_id: Person.where(["(id > 0) AND (name LIKE ? OR nick like ?)","%#{search}%","%#{search}%"]).order(:birthday)) : Coach.none
+      search.length>0 ? Coach.where(person_id: Person.where(["(id > 0) AND (name LIKE ? OR nick like ?)","%#{search}%","%#{search}%"]).order(:birthday)) : Coach.where(person_id: Person.real.order(:birthday))
 		else
-      Coach.where(person_id: Person.real.order(:birthday))
+      Coach.none
 		end
 	end
 end
