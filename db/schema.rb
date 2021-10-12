1c74ac73a92ab743267f3bd6e767ce79ef5b6923
iangullo <iangullo@gmail.com>
2021-10-12 12:51:29 +0200

    Added admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -190,6 +190,7 @@ ActiveRecord::Schema.define(version: 2021_10_07_141917) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.bigint "person_id", default: 0, null: false
+    t.boolean "admin", default: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
