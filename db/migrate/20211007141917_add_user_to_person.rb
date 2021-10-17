dce097c0233d37424fdf4d3898fb5321461d682c
iangullo <iangullo@gmail.com>
2021-10-17 13:12:08 +0200

    Bugfix in migration to cerate fake & initial admin users.

--- db/migrate/20211007141917_add_user_to_person.rb
+++ db/migrate/20211007141917_add_user_to_person.rb
@@ -1,5 +1,7 @@
 class AddUserToPerson < ActiveRecord::Migration[6.1]
   def change
     add_reference :people, :user, null: false, foreign_key: true, default: 0
+    User.create(email: 'admin@bclub.org', password: 'bclub-admin', password_confirmation: 'bclub-admin', person_id: 1)
+    User.last.person=Person.find_by(email: 'admin@bclub.org')
   end
 end
