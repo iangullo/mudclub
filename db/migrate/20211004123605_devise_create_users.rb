94940fffdbeffbacbcc9ca4fb4b50748cc77804d
iangullo <iangullo@gmail.com>
2021-10-17 12:41:40 +0200

    Moved initial user creation after adding reference in people table

--- db/migrate/20211004123605_devise_create_users.rb
+++ db/migrate/20211004123605_devise_create_users.rb
@@ -44,11 +44,5 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
-
-    # additional data to link well in bclub database
-    # fake user_id (0)
-    ActiveRecord::Base.connection.execute("INSERT INTO users (id, email, person_id, created_at, updated_at) values (0,'fake@bclub.org',0,'2021-09-13 08:12','2021-09-13 08:12')")
-    User.create(email: 'admin@bclub.org', password: 'bclub-admin', password_confirmation: 'bclub-admin', person_id: 1)
-#    User.last.person=Person.find_by(email: 'admin@bclub.org')
   end
 end
--- db/migrate/20211007141917_add_user_to_person.rb
+++ db/migrate/20211007141917_add_user_to_person.rb
@@ -1,5 +1,11 @@
 class AddUserToPerson < ActiveRecord::Migration[6.1]
   def change
     add_reference :people, :user, null: false, foreign_key: true, default: 0
+
+    # additional data to link well in bclub database
+    # fake user_id (0)
+    ActiveRecord::Base.connection.execute("INSERT INTO users (id, email, person_id, created_at, updated_at) values (0,'fake@bclub.org',0,'2021-09-13 08:12','2021-09-13 08:12')")
+    User.create(email: 'admin@bclub.org', password: 'bclub-admin', password_confirmation: 'bclub-admin', person_id: 1)
+    User.last.person=Person.find_by(email: 'admin@bclub.org')
   end
 end
