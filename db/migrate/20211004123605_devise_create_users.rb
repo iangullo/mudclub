e746a94c7fcec168e5d854bf9ebfc910f95437c5
iangullo <iangullo@gmail.com>
2021-10-17 12:38:57 +0200

    Attempt to fix initial admin bclub user

--- db/migrate/20211004123605_devise_create_users.rb
+++ db/migrate/20211004123605_devise_create_users.rb
@@ -49,6 +49,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
     # fake user_id (0)
     ActiveRecord::Base.connection.execute("INSERT INTO users (id, email, person_id, created_at, updated_at) values (0,'fake@bclub.org',0,'2021-09-13 08:12','2021-09-13 08:12')")
     User.create(email: 'admin@bclub.org', password: 'bclub-admin', password_confirmation: 'bclub-admin', person_id: 1)
-    User.last.person=Person.find_by(email: 'admin@bclub.org')
+#    User.last.person=Person.find_by(email: 'admin@bclub.org')
   end
 end
