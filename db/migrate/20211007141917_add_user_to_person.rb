cb75e5b706113de76347a5f12be7707fffdd828e
iangullo <iangullo@gmail.com>
2021-10-17 12:42:15 +0200

    + Moved creation of initial users after links are added to people.

--- db/migrate/20211007141917_add_user_to_person.rb
+++ db/migrate/20211007141917_add_user_to_person.rb
@@ -4,8 +4,8 @@ class AddUserToPerson < ActiveRecord::Migration[6.1]
 
     # additional data to link well in bclub database
     # fake user_id (0)
-    ActiveRecord::Base.connection.execute("INSERT INTO users (id, email, person_id, created_at, updated_at) values (0,'fake@bclub.org',0,'2021-09-13 08:12','2021-09-13 08:12')")
-    User.create(email: 'admin@bclub.org', password: 'bclub-admin', password_confirmation: 'bclub-admin', person_id: 1)
+    ActiveRecord::Base.connection.execute("INSERT INTO users (id, email, role, person_id, created_at, updated_at) values (0,'fake@bclub.org',0,0,'2021-09-13 08:12','2021-09-13 08:12')")
+    User.create(email: 'admin@bclub.org', role: :admin, password: 'bclub-admin', password_confirmation: 'bclub-admin', person_id: 1)
     User.last.person=Person.find_by(email: 'admin@bclub.org')
   end
 end
