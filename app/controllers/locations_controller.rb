76a53e560d88a62f76e9bcda53492dc4c63a6bba
iangullo <iangullo@gmail.com>
2022-03-24 12:52:38 +0100

    + Different bugfixes to locations views & controller.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,19 +1,29 @@
 class LocationsController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => [:create, :new, :update, :check_reload]
-  before_action :set_location, only: [:index, :show, :edit, :new, :update, :destroy, :locations]
+  before_action :set_locations, only: [:index, :show, :edit, :new, :update, :destroy, :locations]
 
   # GET /locations
   # GET /locations.json
   def index
-    if current_user.present? and current_user.admin?
+    set_locations unless @locations
+    if current_user.present? and (current_user.admin? or current_user.is_coach)
 		else
 			redirect_to "/"
 		end
   end
 
+  # GET /locations/1
+  # GET /locations/1.json
+  def show
+    if current_user.present? and (current_user.admin? or current_user.is_coach)
+    else
+			redirect_to "/"
+    end
+  end
+
   # GET /locations/1/edit
   def edit
-    if current_user.present? and current_user.admin?
+    if current_user.present? and (current_user.admin? or current_user.is_coach)
 			@location = Location.new(name: "NUEVA") unless @location
 		else
 			redirect_to "/"
@@ -22,7 +32,7 @@ class LocationsController < ApplicationController
 
   # GET /locations/new
   def new
-    if current_user.present? and current_user.admin?
+    if current_user.present? and (current_user.admin? or current_user.is_coach)
       @location = Location.new(name: "NUEVA") unless @location
     else
 			redirect_to "/"
@@ -32,15 +42,15 @@ class LocationsController < ApplicationController
   # POST /locations
   # POST /locations.json
   def create
-    if current_user.present? and current_user.admin?
+    if current_user.present? and (current_user.admin? or current_user.is_coach)
 	    respond_to do |format|
-	      rebuild_location
-        if @location.exists?
-           format.html { redirect_to @location, notice: 'Ya existía esta ubicación.'}
+	      @location = rebuild_location
+        if @location.id!=nil
+           format.html { redirect_to locations_path, notice: 'Ya existía esta ubicación.', action: :index }
            format.json { render :show, :created, location: @location }
         else
           if @location.save
-            format.html { redirect_to locations_url, notice: 'Pista añadida.' }
+            format.html { redirect_to locations_path, notice: 'Pista añadida.' }
 	          format.json { render :index, status: :created, location: locations_url }
           else
             format.html { render :new }
@@ -56,19 +66,19 @@ class LocationsController < ApplicationController
   # PATCH/PUT /locations/1
   # PATCH/PUT /locations/1.json
   def update
-		if current_user.present? and current_user.admin?
+		if current_user.present? and (current_user.admin? or current_user.is_coach)
       respond_to do |format|
-        rebuild_location
-      	if @location.update(location_params)
-	        format.html { redirect_to locations_url, notice: "Pistas actualizadas." }
-					format.json { render :index, status: :created, location: locations_url }
-	      else
-	        format.html { redirect_to :edit }
-	        format.json { render json: @location.errors, status: :unprocessable_entity }
-	      end
-			end
-		else
-			redirect_to "/"
+        @location = rebuild_location
+        if @location.id!=nil and @location.update(location_params)
+  	      format.html { redirect_to locations_path, notice: "Pista actualizadas.", action: :index }
+  				format.json { render :index, status: :created, location: locations_path }
+        else
+          format.html { redirect_to edit_location_path(@location) }
+          format.json { render json: @location.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      redirect_to "/"
     end
   end
 
@@ -78,8 +88,7 @@ class LocationsController < ApplicationController
 		if current_user.present? and current_user.admin?
 			@location.delete
 	    respond_to do |format|
-
-	      format.html { redirect_to locations_url, notice: 'Pista elminada.' }
+	      format.html { redirect_to locations_path, notice: 'Pista elminada.' }
 	      format.json { head :no_content }
 	    end
 		else
@@ -90,16 +99,16 @@ class LocationsController < ApplicationController
 private
   # Never trust parameters from the scary internet, only allow the white list through.
   def location_params
-    params.require(:location).permit(:id, :name, :start, :end, seasons: [], season_locations: [])
+    params.require(:location).permit(:id, :name, :gmaps_url, :practice_court, seasons: [], season_locations: [])
   end
 
-  def set_location
+  def set_locations
     if params[:season_id]
       @season = Season.find(params[:season_id]) unless @season.try(:id)==params[:season_id]
-      @locations = @season.locations
-      @eligible_locations = @season.eligible_locations
+      @locations = @season.locations.order(:name)
+      @eligible_locations = @season.eligible_locations.order(:name)
     else
-      @locations = Location.real
+      @locations = Location.real.order(:name)
     end
     if params[:id]
       @location = Location.find(params[:id]) unless @location.try(:id)==params[:id]
@@ -108,10 +117,14 @@ private
 
   def rebuild_location
     if params[:location]
-      @location                = Location.new
-      @location.name           =  params[:location][:name]
-      @location.gmaps_url      =  params[:location][:gmaps_url]
-      @location.practice_court = (params[:location][:practice_court] == "1")
+      loc                = Location.new
+      loc.name           = params[:location][:name]
+      loc.exists? # reload from database
+      loc.gmaps_url      = params[:location][:gmaps_url]
+      loc.practice_court = (params[:location][:practice_court] == "1")
+      loc
+    else
+      nil
     end
   end
 end
