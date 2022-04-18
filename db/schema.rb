7965d6a4f1410f418f23ed5507221f86686cf69e
iangullo <87307767+iangullo@users.noreply.github.com>
2022-04-18 11:04:28 +0200

    enable unaccent in schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2022_04_17_150755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
