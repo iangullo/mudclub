07125ee64daf2ba4e4c2efb92d2c1e6e9f3fc0e1
iangullo <iangullo@gmail.com>
2022-04-30 11:33:31 +0200

    + Enabled omni_search in drills:   * regular search:  name/description.   * kind: k=<kind>   * target: t=<target>   * skill: s=<skill>

--- app/controllers/drills_controller.rb
+++ app/controllers/drills_controller.rb
@@ -158,56 +158,6 @@ class DrillsController < ApplicationController
 		}
 	end
 
-	# search all drills for specific subsets
-	# NOT WORKING!! - disable for now
-	def search(search=nil)
-		if search
-			if search.length > 0
-				res = search_skill(search)
-				res = search_kind(res, search)
-				res = search_name(res, search)
-			else
-				res = Drill.all
-			end
-		else
-			res = Drill.all
-		end
-		res.order(:kind_id)
-	end
-
-	# filter for fundamentals
-	def search_skill(res=Drill.all, search)
-		s_s = search.scan(/f:(\w+)/)
-		if s_s # matched something
-			return s_s.empty? ? res : Skill.search_drills(s_s.first.first)
-		else
-			return res
-		end
-	end
-
-	# filter drills by kind
-	def search_kind(res=Drill.all, search)
-		s_k = search.scan(/t:(\w+)/)
-		if s_k	# matched something
-			res = s_k.empty? ? res : res.where(kind_id: Kind.find_by(name: s_k.first.first).id)
-		else
-			return res
-		end
-	end
-
-	# filter by name/description
-	def search_name(res=Drill.all, search)
-		s_n = search.scan(/\s*(.+)\sf:\w+|\st:\w+/)
-		if s_n # matched something
-		unless s_n.empty?
-					s_n = s_n.first.first
-				res = res.where("unaccent(name) ILIKE unaccent(?) OR unaccent(description) ILIKE unaccent(?)","%#{s_n}%","%#{s_n}%")
-			else
-				return res
-			end
-		end
-	end
-
 	# Use callbacks to share common setup or constraints between actions.
 	def set_drill
 		@drill = Drill.find(params[:id]) unless @drill.try(:id)==params[:id]
--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,4 +1,6 @@
 class Kind < ApplicationRecord
 	has_many :drills
 	before_save { self.name = self.name.mb_chars.titleize }
+	scope :search, -> (s_k) { where("unaccent(name) ILIKE unaccent(?)","%#{s_k}%") }
+
 end
