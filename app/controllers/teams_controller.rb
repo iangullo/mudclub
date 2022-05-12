8e1839f29db6ecb35c0d720470429e813f5e5823
iangullo <iangullo@gmail.com>
2022-05-12 18:39:25 +0200

    + Minor bugfix in Team roster view - bad call to populate grid

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -45,7 +45,7 @@ class TeamsController < ApplicationController
 			end
 			@header = header_fields(@team.to_s)
 			@header << [{kind: "icon", value: "player.svg", size: "30x30"}, {kind: "label", value: I18n.t(:l_roster_show)}]
-			@grid   =  players_grid(players: @team.players.order(:number), obj: @team)
+			@grid   =  player_grid(players: @team.players.order(:number), obj: @team)
 		else
 			redirect_to "/"
 		end
