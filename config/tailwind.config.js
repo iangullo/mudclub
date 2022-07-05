4759044f2e46916b5f5aa07ab3734b151e92858c
iangullo <iangullo@gmail.com>
2022-07-05 17:31:07 +0200

    Updated importmap / tailwind configuration to use flowbitewq

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
-    './app/components/*.{erb,haml,html,slim}',
+    './app/components/**/*.{erb,haml,html,slim}',
     './node_modules/flowbite/**/*.js'
   ],
   theme: {
