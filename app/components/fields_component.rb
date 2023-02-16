ea2a24328a21ed15fa5fac79df3a7829569b53e8
iangullo <iangullo@gmail.com>
2023-02-16 09:30:02 +0100

    + FieldsComponent: rearranged code iordering case options alphabetically - easier to maintain..

--- app/components/fields_component.rb
+++ app/components/fields_component.rb
@@ -65,30 +65,41 @@ class FieldsComponent < ApplicationComponent
 			res << [] # new row n header
 			row.each do |item|
 				case item[:kind]
-				when "icon"
-					item[:align] = "right" unless item[:align]
-					item[:class] = item[:class] ? item[:class] + " align-middle" : "align-middle"
-					item[:size]  = "25x25" unless item[:size]
+				when "accordion"
+					item[:h_class] = "font-semibold text-left text-indigo-900"
+					item[:t_class] = "font-semibold text-right text-indigo-900"
+					item[:i_class] = "flex justify-between items-center p-1 w-full bg-gray-100 text-left text-gray-700 rounded-md hover:bg-gray-500 hover:text-indigo-100 focus:bg-indigo-900 focus:text-gray-200"
+					i = 1
+					item[:objects].each { |obj|
+						obj[:head_id] = "accordion-collapse-heading-" + i.to_s
+						obj[:body_id] = "accordion-collapse-body-" + i.to_s
+						i = i +1
+					}
+				when "gap"
+					item[:size]  = 4 unless item[:size]
 				when "header-icon"
 					item[:align] = "center"
 					item[:class] = item[:class] ? item[:class] + " align-top" : "align-top"
 					item[:size]  = "50x50" unless item[:size]
 					item[:rows]  = 2 unless item[:rows]
-				when "title"
-					item[:class] = "align-top font-bold text-yellow-600"
-				when "subtitle"
-					item[:class] = "align-top font-bold"
+				when "icon"
+					item[:align] = "right" unless item[:align]
+					item[:class] = item[:class] ? item[:class] + " align-middle" : "align-middle"
+					item[:size]  = "25x25" unless item[:size]
+				when "icon-label"
+					item[:size]  = "25x25" unless item[:size]
+					item[:class] = "align-top inline-flex"
 				when "label", "label-checkbox"
 					item[:class]   = item[:class] ? item[:class] + " inline-flex align-top font-semibold" : " inline-flex align-top font-semibold"
 					item[:i_class] = "rounded bg-gray-200 text-blue-700"
+				when "lines"
+					item[:class] = "align-top border px py" unless item[:class]
 				when "link"
 					item[:class]   = item[:class] ? item[:class] : " inline-flex align-middle p-0 text-sm"
 					item[:size]    = "20x20" unless item[:size]
 				when "location"
 					item[:class]   = "inline-flex align-top font-semibold"
 					item[:i_class] = "rounded-md hover:bg-blue-100"
-				when "string"
-					item[:class] = "align-top"
 				when /^(search-.+)$/
 					item[:align]   = "left" unless item[:align]
 					item[:size]    = 16 unless item[:size]
@@ -99,18 +110,17 @@ class FieldsComponent < ApplicationComponent
 					item[:l_class] = "absolute text-md font-semibold text-gray-700 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-transparent px-0 peer-focus:px-0 peer-focus:text-blue-700 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-0"
 					item[:fields]  = [{kind: item[:kind], key: item[:key].to_sym, label: item[:label], options: item[:options], value: item[:value]}] unless item[:kind] == "search-combo"
 					item[:kind]    = "search-combo"
-				when "icon-label"
-					item[:size]  = "25x25" unless item[:size]
-					item[:class] = "align-top inline-flex"
-				when "gap"
-					item[:size]  = 4 unless item[:size]
 				when "side-cell"
 					item[:align] = "right" unless item[:align]
 					item[:class] = "align-center font-semibold text-indigo-900"
+				when "string"
+					item[:class] = "align-top"
+				when "subtitle"
+					item[:class] = "align-top font-bold"
+				when "title"
+					item[:class] = "align-top font-bold text-yellow-600"
 				when "top-cell"
 					item[:class] = "font-semibold bg-indigo-900 text-gray-300 align-center border px py"
-				when "lines"
-					item[:class] = "align-top border px py" unless item[:class]
 				when "upload"
 					item[:class] = "align-middle px py" unless item[:class]
 					item[:i_class] = "inline-flex align-center rounded-md shadow bg-gray-100 ring-2 ring-gray-300 hover:bg-gray-300 focus:border-gray-300 font-semibold text-sm whitespace-nowrap px-1 py-1 m-1 max-h-6 max-w-6 align-center"
@@ -122,16 +132,6 @@ class FieldsComponent < ApplicationComponent
 						item[:max]     = 99 unless item[:max]
 						item[:step]    = 1 unless item[:step]
 					end
-				when "accordion"
-					item[:h_class] = "font-semibold text-left text-indigo-900"
-					item[:t_class] = "font-semibold text-right text-indigo-900"
-					item[:i_class] = "flex justify-between items-center p-1 w-full bg-gray-100 text-left text-gray-700 rounded-md hover:bg-gray-500 hover:text-indigo-100 focus:bg-indigo-900 focus:text-gray-200"
-					i = 1
-					item[:objects].each { |obj|
-						obj[:head_id] = "accordion-collapse-heading-" + i.to_s
-						obj[:body_id] = "accordion-collapse-body-" + i.to_s
-						i = i +1
-					}
 				else
 					item[:i_class] = "rounded p-0" unless item[:kind]=="gap"
 				end
