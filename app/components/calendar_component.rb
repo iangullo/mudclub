8bbebed08130e24d57398dd545c24601def3b831
iangullo <iangullo@gmail.com>
2023-04-24 15:49:44 +0200

    BUGFIX:   + CalendarComponent: avoid crash if no events exist.

--- app/components/calendar_component.rb
+++ app/components/calendar_component.rb
@@ -162,22 +162,22 @@ class CalendarComponent < ApplicationComponent
 
 		# return backbutton if we do not exceed beginning of events season
 		def set_back_button(events:)
-			if events # we can check for a starting point of the season
-				if (e_date = events.first.team.season.start_date)
-					c_date = @cells[1][1][:date]
-					return nil if c_date <= e_date	# we have reached beginning of season
-				end
+			e_first = events.try(:first)	# we have a first event?
+			e_date  = e_first ? e_first.team.season.start_date : Season.latest.start_date
+			if e_date
+				c_date = @cells[1][1][:date]
+				return nil if c_date <= e_date	# we have reached beginning of season
 			end
 			ButtonComponent.new(button: {kind: "back", label: "", url: @anchor.split('?').first + "?start_date=" + (@start_date - 1.month).to_s})
 		end
 
 		# return fwdbutton depending on end_date
 		def set_fwd_button(events:)
-			if events # we can check for a starting point of the season
-				if (e_date = events.last.team.season.end_date)
-					c_date = @cells.last.last[:date]
-					return nil if c_date >= e_date	# we have reached end of season
-				end
+			e_last = events.try(:last)# we have a last event?
+			e_date = e_last ? e_last.team.season.end_date : Season.latest.end_date
+			if e_date
+				c_date = @cells.last.last[:date]
+				return nil if c_date >= e_date	# we have reached end of season
 			end
 			ButtonComponent.new(button: {kind: "forward", label: "", url: @anchor.split('?').first + "?start_date=" + (@start_date + 1.month).to_s})
 		end
