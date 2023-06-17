1210da03fbac6e37887ea1256e830c1c2e121155
iangullo <iangullo@gmail.com>
2023-06-17 12:46:12 +0200

    Improved SearchBox bnehaviour for select boxes -show as selected the current value, if possible.

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -27,7 +27,7 @@ class SeasonsController < ApplicationController
 			@events = Event.short_term.for_season(@season).non_training
 			title   = helpers.season_title_fields(title: I18n.t("season.single"), cols: 2)
 			title << [
-				{kind: "search-collection", key: :search, url: seasons_path, options: Season.real.order(start_date: :desc)},
+				{kind: "search-collection", key: :search, url: seasons_path, options: Season.real.order(start_date: :desc), value: @season},
 				helpers.button_field({kind: "add", url: new_season_path, label: I18n.t("action.create"), frame: "modal"})
 			]
 			@fields = create_fields(title)
