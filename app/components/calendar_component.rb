1678713f3172e1935e44d107b13534f9cf94109c
iangullo <iangullo@gmail.com>
2023-07-26 14:11:31 +0200

    all calendar event views by Users with @player role, have to be modal.

--- app/components/calendar_component.rb
+++ app/components/calendar_component.rb
@@ -97,7 +97,8 @@ class CalendarComponent < ApplicationComponent
 
 		# determine event_color & url depending on event kind and parameters
 		def c_event_init(event:)
-			c_event = {id: event.id}
+			c_event        = {id: event.id}
+			c_event[:data] = {turbo_frame: "modal"} if @user.player? || event.rest?
 			case event.kind
 			when "match"
 				sc = event.total_score	# our team first
@@ -114,12 +115,10 @@ class CalendarComponent < ApplicationComponent
 			when "train"
 				c_event[:icon]  = "training.svg"
 				c_event[:label] = event.to_s
-				c_event[:data]  = {turbo_frame: "modal"	} if @user.player?
 				b_color         = "blue"
 			when "rest"
 				c_event[:icon]  = "rest.svg"
 				c_event[:label] = event.to_s
-				c_event[:data]  = {turbo_frame: "modal"	}
 				b_color         = "gray"
 			end
 			c_event[:b_class] = "bg-#{b_color}-300 rounded-lg border px py hover:text-white hover:bg-#{b_color}-700 text-sm"
