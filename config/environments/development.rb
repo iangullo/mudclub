1adcc918897aa19eb9ee9750f6da11ad64db821b
iangullo <iangullo@gmail.com>
2024-01-31 08:40:09 +0100

    remove custom host name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,7 +77,6 @@ Rails.application.configure do
     IPAddr.new("0.0.0.0/0"),        # All IPv4 addresses.
     IPAddr.new("::/0"),             # All IPv6 addresses.
     "localhost",                    # The localhost reserved domain.
-    "riddick",                      # The localhost reserved domain.
     ENV["RAILS_DEVELOPMENT_HOSTS"]  # Additional comma-separated hosts for development.
   ]  
 end
