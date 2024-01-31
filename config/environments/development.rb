5677a3cdd34c0b014a024cc76999cfb2bdedc532
iangullo <iangullo@gmail.com>
2024-01-31 08:44:31 +0100

    DEBUG: remove access to all IPs in development mode, only those clients  specified in RAILS_DEVELOPMENT_HOSTS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,8 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   Rails.application.config.hosts = [
-    IPAddr.new("0.0.0.0/0"),        # All IPv4 addresses.
-    IPAddr.new("::/0"),             # All IPv6 addresses.
     "localhost",                    # The localhost reserved domain.
     ENV["RAILS_DEVELOPMENT_HOSTS"]  # Additional comma-separated hosts for development.
   ]  
