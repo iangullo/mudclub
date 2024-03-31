fbd6d14341c0c05eccb4c0f5879b871cb26e0d68
iangullo <iangullo@gmail.com>
2024-03-31 11:55:55 +0200

    Added spearator lines to mark beginning/end of @drill explanation content in  Drill pdf export.

--- app/controllers/drills_controller.rb
+++ app/controllers/drills_controller.rb
@@ -162,7 +162,9 @@ class DrillsController < ApplicationController
 			pdf_label_text(label: I18n.t("drill.name"), text: @drill.name)
 			pdf_label_text(label: I18n.t("drill.desc"), text: @drill.description) if @drill.description.present?
 			pdf_label_text(label: I18n.t("target.many"), text: @drill.print_targets(array: false))
+			pdf_separator_line
 			pdf_rich_text(@drill.explanation) if @drill&.explanation&.present?
+			pdf_separator_line
 			pdf_label_text(label: I18n.t("skill.many"), text: @drill.print_skills)
 			pdf
 		end
