8e5d1d5731d5dcdf490de4086eea840868ec4efe
iangullo <iangullo@gmail.com>
2024-04-05 09:10:38 +0200

    CalednarComponent: Formatting & Tweak

--- app/components/calendar_component.rb
+++ app/components/calendar_component.rb
@@ -43,97 +43,6 @@ class CalendarComponent < ApplicationComponent
 	end
 
 	private
-		# prepare row/column canvas of @cells depending on
-		# calendar view start_date:
-		def new_canvas(start_date:)
-			@c_month = start_date.month
-			@c_cols  = 7	# 7 days per week
-			f_date = start_date.at_beginning_of_month
-			f_date = f_date.prev_occurring(:monday) unless f_date.wday==1
-			l_date = start_date.at_end_of_month
-			l_date = l_date.next_occurring(:sunday) unless l_date.wday==7
-			@c_rows = ((l_date - f_date)/@c_cols).ceil
-			@dayname = []	# host day names
-			1.upto(@c_cols) { |j| @dayname << I18n.t("calendar.daynames_a")[j] if @c_cols > 1 }
-			@cells   = canvas_cells(f_date:)
-		end
-
-		# cells of canvas have strong dependency on calendar scope
-		def canvas_cells(f_date:)
-			@cells = []	# prepare cell canvas (organized as [row][column])
-			1.upto(@c_rows) do |i| # one cell per day
-				row = []	# new calendar row
-				1.upto(@c_cols) do |j|
-					in_scope = (f_date.month==@start_date.month)
-					n_cell = {c_class: (in_scope ? @iscope_cls : @oscope_cls), date: f_date, events: []}
-					row[j] = n_cell	# add new cell to the row
-					f_date = f_date + 1 # check next day
-				end
-				@cells[i] = row	# add row
-			end
-			@cells
-		end
-
-		# Parse a collection of Events and map to the canvas
-		def parse_events(obj: nil, create_url: nil)
-			@events.each do |event|	# find the day for each event
-				e_cell = get_cell(e_date: event.start_date)
-				if e_cell
-					c_event = c_event_init(event:)
-					e_cell[:events] << c_event
-				end
-			end if @events
-			add_empty_buttons(obj:, create_url:) if obj and @user and create_url
-		end
-
-		# returns cell for a specific date
-		def get_cell(e_date:)
-			1.upto(@c_rows) do |i|
-				@cells[i].each do |e_cell|
-					return e_cell if e_cell and e_cell[:date]==e_date # found it!
-				end
-			end
-			return nil
-		end
-
-		# determine event_color & url depending on event kind and parameters
-		def c_event_init(event:)
-			c_event        = {id: event.id}
-			c_event[:url]  = "/events/#{event[:id]}/?cal=true"
-			c_event[:url] += "&rdx=#{@rdx}" if @rdx
-			case event.kind
-			when "match"
-				sc = event.total_score	# our team first
-				c_event[:icon] = "match.svg"
-				c_event[:home] = event.home? ? sc[:ours] : sc[:opps]
-				c_event[:away] = event.home? ? sc[:opps] : sc[:ours]
-				if sc[:ours][:points] > sc[:opps][:points]
-					b_color = "green"
-				elsif sc[:ours][:points] < sc[:opps][:points]
-					b_color = "red"
-				else
-					b_color = "yellow"
-				end
-			when "train"
-				c_event[:icon]  = "training.svg"
-				c_event[:label] = event.to_s
-				if event.has_player(@user.player&.id)
-					c_event[:url]  = "/events/#{event[:id]}/player_stats?retlnk=#{@anchor}"
-					c_event[:data] = {turbo_frame: "modal"}
-				end
-				b_color         = "blue"
-			when "rest"
-				c_event[:icon]  = "rest.svg"
-				c_event[:label] = event.to_s
-				c_event[:data]  = {turbo_frame: "modal"}
-				b_color         = "gray"
-			end
-			c_event[:b_class] = "bg-#{b_color}-300 rounded-lg border px py hover:text-white hover:bg-#{b_color}-700 text-sm"
-			c_event[:t_class] = "bg-#{b_color}-700 rounded-lg invisible inline-block font-light border border-gray-200 border shadow-sm m-2 text-white text-sm z-10"
-			c_event[:l_class] = "hover:text-white hover:bg-#{b_color}-700"
-			c_event
-		end
-
 		# add create event buttons to empty canvas cells
 		def add_empty_buttons(obj:, create_url:)
 			@add_btn   = []
@@ -179,6 +88,97 @@ class CalendarComponent < ApplicationComponent
 			return res
 		end
 
+		# determine event_color & url depending on event kind and parameters
+		def c_event_init(event:)
+			c_event        = {id: event.id}
+			c_event[:url]  = "/events/#{event[:id]}/?cal=true"
+			c_event[:url] += "&rdx=#{@rdx}" if @rdx
+			case event.kind
+			when "match"
+				sc = event.total_score	# our team first
+				c_event[:icon] = "match.svg"
+				c_event[:home] = event.home? ? sc[:ours] : sc[:opps]
+				c_event[:away] = event.home? ? sc[:opps] : sc[:ours]
+				if sc[:ours][:points] > sc[:opps][:points]
+					b_color = "green"
+				elsif sc[:ours][:points] < sc[:opps][:points]
+					b_color = "red"
+				else
+					b_color = "yellow"
+				end
+			when "train"
+				c_event[:icon]  = "training.svg"
+				c_event[:label] = event.to_s
+				if event.has_player(@user.player&.id)
+					c_event[:url]  = "/events/#{event[:id]}/player_stats?retlnk=#{@anchor}"
+					c_event[:data] = {turbo_frame: "modal"}
+				end
+				b_color         = "blue"
+			when "rest"
+				c_event[:icon]  = "rest.svg"
+				c_event[:label] = event.to_s
+				c_event[:data]  = {turbo_frame: "modal"}
+				b_color         = "gray"
+			end
+			c_event[:b_class] = "bg-#{b_color}-300 rounded-lg border px py hover:text-white hover:bg-#{b_color}-700 text-sm"
+			c_event[:t_class] = "bg-#{b_color}-700 rounded-lg invisible inline-block font-light border border-gray-200 border shadow-sm m-2 text-white text-sm z-10"
+			c_event[:l_class] = "hover:text-white hover:bg-#{b_color}-700"
+			c_event
+		end
+
+		# cells of canvas have strong dependency on calendar scope
+		def canvas_cells(f_date:)
+			@cells = []	# prepare cell canvas (organized as [row][column])
+			1.upto(@c_rows) do |i| # one cell per day
+				row = []	# new calendar row
+				1.upto(@c_cols) do |j|
+					in_scope = (f_date.month==@start_date.month)
+					n_cell = {c_class: (in_scope ? @iscope_cls : @oscope_cls), date: f_date, events: []}
+					row[j] = n_cell	# add new cell to the row
+					f_date = f_date + 1 # check next day
+				end
+				@cells[i] = row	# add row
+			end
+			@cells
+		end
+
+		# returns cell for a specific date
+		def get_cell(e_date:)
+			1.upto(@c_rows) do |i|
+				@cells[i].each do |e_cell|
+					return e_cell if e_cell and e_cell[:date]==e_date # found it!
+				end
+			end
+			return nil
+		end
+
+		# prepare row/column canvas of @cells depending on
+		# calendar view start_date:
+		def new_canvas(start_date:)
+			@c_month = start_date.month
+			@c_cols  = 7	# 7 days per week
+			f_date = start_date.at_beginning_of_month
+			f_date = f_date.prev_occurring(:monday) unless f_date.wday==1
+			l_date = start_date.at_end_of_month
+			l_date = l_date.next_occurring(:sunday) unless l_date.wday==7
+			@c_rows = ((l_date - f_date)/@c_cols).ceil
+			@dayname = []	# host day names
+			1.upto(@c_cols) { |j| @dayname << I18n.t("calendar.daynames_a")[j] if @c_cols > 1 }
+			@cells   = canvas_cells(f_date:)
+		end
+
+		# Parse a collection of Events and map to the canvas
+		def parse_events(obj: nil, create_url: nil)
+			@events.each do |event|	# find the day for each event
+				e_cell = get_cell(e_date: event.start_date)
+				if e_cell
+					c_event = c_event_init(event:)
+					e_cell[:events] << c_event
+				end
+			end if @events
+			add_empty_buttons(obj:, create_url:) if obj and @user and create_url
+		end
+
 		# return backbutton if we do not exceed beginning of events season
 		def set_back_button
 			if @s_date
@@ -188,6 +188,17 @@ class CalendarComponent < ApplicationComponent
 			ButtonComponent.new(button: {kind: "back", label: "", url: anchor_url(@start_date - 1.month)})
 		end
 
+		# define the first valid calendar date for the parent object (team/Club)
+		def set_date_limits(obj:)
+			if obj.is_a?(Team)
+				season = obj.season
+			else # let's try to get it from the events list
+				season = @events&.empty? ? Season.latest : @events.last.team.season
+			end
+			@s_date = season.start_date
+			@e_date = season.end_date
+		end
+
 		# return fwdbutton depending on end_date
 		def set_fwd_button
 			if @e_date
@@ -195,16 +206,5 @@ class CalendarComponent < ApplicationComponent
 				return nil if c_date >= @e_date	# we have reached end of season
 			end
 			ButtonComponent.new(button: {kind: "forward", label: "", url: anchor_url(@start_date + 1.month)})
-		end
-
-		# define the first valid calendar date for the parent object (team/Club)
-		def set_date_limits(obj:)
-			if obj.is_a?(Team)
-				season = obj.season
-			else # let's try to get it from the events list
-				season = @events&.empty? ? Season.latest : @events.first.team.season
-			end
-			@s_date = season.start_date
-			@e_date = season.end_date
 		end
 end
